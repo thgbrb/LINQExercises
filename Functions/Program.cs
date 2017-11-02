6196ba7c3d300b88a8f2ab3a57ab73dbaf526520
Thiago <thiago.borba@icloud.com>
2017-11-01 23:05:26 -0200

    First commit

---
+++ Functions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using static Functions.FunctionsDefinition;
+using static System.Console;
+
+namespace Functions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define a string arrays
+            var numbers = new[] { "4", "2", "1", "7", "8", "9", "34", "10", };
+          
+            numbers
+                .Select(str2int) // For each list element apply func str2int
+                .ToList() // Create a new list
+                .ForEach(x => WriteLine($@"{x.GetType().Name} : {x.ToString()}")); // For each element print it
+        }
+    }
+
+    /// <summary>
+    /// Functions
+    /// </summary>
+    public static class FunctionsDefinition
+    {
+        /// <summary>
+        /// Convert string to int
+        /// </summary>
+        public static Func<string, int> str2int =
+            (s) =>
+            {
+                int i;
+                int.TryParse(s, out i);
+
+                return i;
+            };
+    }
+}
