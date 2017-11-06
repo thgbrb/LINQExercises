f4833075339194ae4f314f65744ba9e3cdc960b7
Thiago <thiago.borba@icloud.com>
2017-11-06 21:13:51 -0200

    Variable into linq expressions with Let

---
+++ LinqAndLet/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using System.Linq;
+
+namespace LinqAndLet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 100);
+
+            var result = from i in numbers
+                         let x = i * 2
+                         select x;
+
+            result.ToList().ForEach(x => WriteLine(x.ToString()));
+            
+            ReadLine();
+        }
+    }
+}
